7e5c92cf1012d7c80729e1ad3c91e240ea56e157
Yaroslav-Rakov <cnxlan4@gmail.com>
2021-08-13 11:19:24 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import Vuelidate from 'vuelidate'
+import Login from './views/Login.vue'
+
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
+
+// Register component globally
+Vue.component(
+    'LoginComponent', Login
+  
+)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
